5cf9f3ec8f2e6d3f1ca8cc85429e230017a830c5
Nisarg Patel <ntpatel2411@gmail.com>
2024-06-23 14:17:19 +0530

    Styling

--- src/components/comman/form-button.tsx
+++ src/components/comman/form-button.tsx
@@ -1,16 +1,20 @@
-'use client';
+"use client";
 
-import {useFormStatus} from 'react-dom';
-import { Button } from '@nextui-org/react';
+import { useFormStatus } from "react-dom";
+import { Button } from "@nextui-org/react";
 
 interface FormButtonProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
-export default function FormButton({children}: FormButtonProps) {
-    const {pending} = useFormStatus();
-    return (
-        <Button type="submit" isLoading={pending} className='w-full'>
-            {children}
-        </Button>
-    );
+export default function FormButton({ children }: FormButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type="submit"
+      isLoading={pending}
+      className="w-full bg-gradient-to-tr from-blue-500 to-purple-500 text-white shadow-lg"
+    >
+      {children}
+    </Button>
+  );
 }
